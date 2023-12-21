168c6c1f048b22de340d949cc641421b522cff08
shedivesin <>
2023-12-20 21:17:53 -0600

    Make incremental, more numerically stable

--- linear_regression.mjs
+++ linear_regression.mjs
@@ -1,7 +1,3 @@
-function identity(x) {
-  return x;
-}
-
 function det2(a, b, c, d) {
   return a * d - b * c;
 }
@@ -10,71 +6,71 @@ function det3(a, b, c, d, e, f, g, h, i) {
   return a * det2(e, f, h, i) - b * det2(d, f, g, i) + c * det2(d, e, g, h);
 }
 
-function regression2(pairs, transform=identity) {
-  const n = pairs.length;
-  let Σx = 0;
-  let Σy = 0;
-  let Σxx = 0;
-  let Σxy = 0;
-
-  for(let i = 0; i < n; i++) {
-    const pair = pairs[i];
-    const x = transform(pair[0]);
-    const y = transform(pair[1]);
-
-    Σx  += x;
-    Σy  += y;
-    Σxx += x * x;
-    Σxy += x * y;
+class Regression2D {
+  constructor() {
+    this.n  = 0;
+    this.x  = 0;
+    this.y  = 0;
+    this.xx = 0;
+    this.xy = 0;
   }
 
-  // Solve the following system of equations using Cramer's rule:
-  //    n a + Σx  b = Σy
-  //   Σx a + Σxx b = Σxy
-  const d = det2( n, Σx, Σx , Σxx);
-  const a = det2(Σy, Σx, Σxy, Σxx) / d;
-  const b = det2( n, Σy, Σx , Σxy) / d;
+  add(x, y) {
+    this.n  ++;
+    this.x  += (x     - this.x ) / this.n;
+    this.y  += (y     - this.y ) / this.n;
+    this.xx += (x * x - this.xx) / this.n;
+    this.xy += (x * y - this.xy) / this.n;
 
-  return [a, b];
-}
-
-function regression3(triplets, transform=identity) {
-  const n = triplets.length;
-  let Σx = 0;
-  let Σy = 0;
-  let Σz = 0;
-  let Σxx = 0;
-  let Σxy = 0;
-  let Σxz = 0;
-  let Σyy = 0;
-  let Σyz = 0;
-
-  for(let i = 0; i < n; i++) {
-    const triplet = triplets[i];
-    const x = transform(triplet[0]);
-    const y = transform(triplet[1]);
-    const z = transform(triplet[2]);
-
-    Σx  += x;
-    Σy  += y;
-    Σz  += z;
-    Σxx += x * x;
-    Σxy += x * y;
-    Σxz += x * z;
-    Σyy += y * y;
-    Σyz += y * z;
+    return this;
   }
 
-  // Solve the following system of equations using Cramer's rule:
-  //    n a + Σx  b + Σy  c = Σz
-  //   Σx a + Σxx b + Σxy c = Σxz
-  //   Σy a + Σxy b + Σyy c = Σyz
-  const d = det3( n, Σx, Σy, Σx , Σxx, Σxy, Σy , Σxy, Σyy);
-  const a = det3(Σz, Σx, Σy, Σxz, Σxx, Σxy, Σyz, Σxy, Σyy) / d;
-  const b = det3( n, Σz, Σy, Σx , Σxz, Σxy, Σy , Σyz, Σyy) / d;
-  const c = det3( n, Σx, Σz, Σx , Σxx, Σxz, Σy , Σxy, Σyz) / d;
+  done() {
+    const d = det2(1, this.x, this.x, this.xx);
 
-  return [a, b, c];
+    return [
+      det2(this.y, this.x, this.xy, this.xx) / d,
+      det2(     1, this.y, this.x , this.xy) / d,
+    ];
+  }
 }
 
-export { regression2, regression3 };
+class Regression3D {
+  constructor() {
+    this.n  = 0;
+    this.x  = 0;
+    this.y  = 0;
+    this.z  = 0;
+    this.xx = 0;
+    this.xy = 0;
+    this.xz = 0;
+    this.yy = 0;
+    this.yz = 0;
+  }
+
+  add(x, y, z) {
+    this.n  ++;
+    this.x  += (x     - this.x ) / this.n;
+    this.y  += (y     - this.y ) / this.n;
+    this.z  += (z     - this.z ) / this.n;
+    this.xx += (x * x - this.xx) / this.n;
+    this.xy += (x * y - this.xy) / this.n;
+    this.xz += (x * z - this.xz) / this.n;
+    this.yy += (y * y - this.yy) / this.n;
+    this.yz += (y * z - this.yz) / this.n;
+
+    return this;
+  }
+
+  done() {
+    const d = det3(1, this.x, this.y, this.x, this.xx, this.xy, this.y, this.xy, this.yy);
+
+    return [
+      det3(this.z, this.x, this.y, this.xz, this.xx, this.xy, this.yz, this.xy, this.yy) / d,
+      det3(     1, this.z, this.y, this.x , this.xz, this.xy, this.y , this.yz, this.yy) / d,
+      det3(     1, this.x, this.z, this.x , this.xx, this.xz, this.y , this.xy, this.yz) / d,
+    ];
+  }
+}
+
+export {Regression2D, Regression3D};
--- test.mjs
+++ test.mjs
@@ -1,5 +1,5 @@
 import assert from "node:assert/strict";
-import {regression2, regression3} from "./linear_regression.mjs";
+import {Regression2D, Regression3D} from "./linear_regression.mjs";
 
 function r1e3(x) {
   return Math.round(x * 1e3) / 1e3;
@@ -7,42 +7,40 @@ function r1e3(x) {
 
 // https://en.wikipedia.org/wiki/Simple_linear_regression#Numerical_example
 assert.deepEqual(
-  regression2(
-    [
-      [1.47, 52.21],
-      [1.50, 53.12],
-      [1.52, 54.48],
-      [1.55, 55.84],
-      [1.57, 57.20],
-      [1.60, 58.57],
-      [1.63, 59.93],
-      [1.65, 61.29],
-      [1.68, 63.11],
-      [1.70, 64.47],
-      [1.73, 66.28],
-      [1.75, 68.10],
-      [1.78, 69.92],
-      [1.80, 72.19],
-      [1.83, 74.46],
-    ],
-  ).map(r1e3),
+  new Regression2D().
+    add(1.47, 52.21).
+    add(1.50, 53.12).
+    add(1.52, 54.48).
+    add(1.55, 55.84).
+    add(1.57, 57.20).
+    add(1.60, 58.57).
+    add(1.63, 59.93).
+    add(1.65, 61.29).
+    add(1.68, 63.11).
+    add(1.70, 64.47).
+    add(1.73, 66.28).
+    add(1.75, 68.10).
+    add(1.78, 69.92).
+    add(1.80, 72.19).
+    add(1.83, 74.46).
+    done().
+    map(r1e3),
   [-39.062, 61.272],
 );
 
 // https://www.statology.org/multiple-linear-regression-by-hand/
 assert.deepEqual(
-  regression3(
-    [
-      [60, 22, 140],
-      [62, 25, 155],
-      [67, 24, 159],
-      [70, 20, 179],
-      [71, 15, 192],
-      [72, 14, 200],
-      [75, 14, 212],
-      [78, 11, 215],
-    ],
-  ).map(r1e3),
+  new Regression3D().
+    add(60, 22, 140).
+    add(62, 25, 155).
+    add(67, 24, 159).
+    add(70, 20, 179).
+    add(71, 15, 192).
+    add(72, 14, 200).
+    add(75, 14, 212).
+    add(78, 11, 215).
+    done().
+    map(r1e3),
   [-6.867, 3.148, -1.656],
 );
 
