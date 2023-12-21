8dfcdeb11e27d6c962ce6db058533e7dc2bbe929
shedivesin <>
2023-12-21 13:16:51 -0600

    Minor refactor in prep for better solvers

--- linear_regression.mjs
+++ linear_regression.mjs
@@ -4,19 +4,50 @@ function identity(x) {
   return x;
 }
 
+// Give the determinant of the matrix:
+//   a c
+//   b d
 function det2(a, b, c, d) {
-  return a * d - b * c;
+  return a * d - c * b;
 }
 
+// Give the determinant of the matrix:
+//   a d g
+//   b e h
+//   c f i
 function det3(a, b, c, d, e, f, g, h, i) {
-  return a * det2(e, f, h, i) - b * det2(d, f, g, i) + c * det2(d, e, g, h);
+  return a * det2(e, f, h, i) - d * det2(b, c, h, i) + g * det2(b, c, e, f);
 }
 
+// Solve the linear system of equations:
+//   a x + c y = e
+//   b x + d y = f
+function solve2(a, b, c, d, e, f) {
+  const det = det2(a, b, c, d);
+  const x = det2(e, f, c, d) / det;
+  const y = det2(a, b, e, f) / det;
+
+  return [x, y];
+}
+
+// Solve the linear system of equations:
+//   a x + d y + g z = j
+//   b x + e y + h z = k
+//   c x + f y + i z = l
+function solve3(a, b, c, d, e, f, g, h, i, j, k, l) {
+  const det = det3(a, b, c, d, e, f, g, h, i);
+  const x = det3(j, k, l, d, e, f, g, h, i) / det;
+  const y = det3(a, b, c, j, k, l, g, h, i) / det;
+  const z = det3(a, b, c, d, e, f, j, k, l) / det;
+
+  return [x, y, z];
+}
+
+// Run an ordinary least squares regression on a + b x = y.
 function regression2(x, y) {
   const n = x.length;
   assert.equal(y.length, n);
 
-  // Collapse inputs into a matrix representation.
   let Σx  = 0;
   let Σy  = 0;
   let Σxx = 0;
@@ -29,22 +60,15 @@ function regression2(x, y) {
     Σxy += x[i] * y[i];
   }
 
-  // Solve the following system of equations using Cramer's rule:
-  //    n a + Σx  b = Σy
-  //   Σx a + Σxx b = Σxy
-  const d = det2( n, Σx, Σx , Σxx);
-  const a = det2(Σy, Σx, Σxy, Σxx) / d;
-  const b = det2( n, Σy, Σx , Σxy) / d;
-
-  return [a, b];
+  return solve2(n, Σx, Σx, Σxx, Σy, Σxy);
 }
 
+// Run a multilinear ordinary least squares regression on a + b x + c y = z.
 function regression3(x, y, z) {
   const n = x.length;
   assert.equal(y.length, n);
   assert.equal(z.length, n);
 
-  // Collapse inputs into a matrix representation.
   let Σx  = 0;
   let Σy  = 0;
   let Σz  = 0;
@@ -65,18 +89,13 @@ function regression3(x, y, z) {
     Σyz += y[i] * z[i];
   }
 
-  // Solve the following system of equations using Cramer's rule:
-  //    n a + Σx  b + Σy  c = Σz
-  //   Σx a + Σxx b + Σxy c = Σxz
-  //   Σy a + Σxy b + Σyy c = Σyz
-  const d = det3( n, Σx, Σy, Σx , Σxx, Σxy, Σy , Σxy, Σyy);
-  const a = det3(Σz, Σx, Σy, Σxz, Σxx, Σxy, Σyz, Σxy, Σyy) / d;
-  const b = det3( n, Σz, Σy, Σx , Σxz, Σxy, Σy , Σyz, Σyy) / d;
-  const c = det3( n, Σx, Σz, Σx , Σxx, Σxz, Σy , Σxy, Σyz) / d;
-
-  return [a, b, c];
+  return solve3(n, Σx, Σy, Σx, Σxx, Σxy, Σy, Σxy, Σyy, Σz, Σxz, Σyz);
 }
 
+// Find the best fit to the function a + b sin(phase + frequency x) = y.
+// Input frequency is required, but input phase may be omitted (in which case a
+// best fit will be found).
+// Returns [a, b>=0, -π<=phase<π, frequency].
 function sinusoidal(x, y, frequency, phase=NaN) {
   const n = x.length;
   assert.equal(y.length, n);
